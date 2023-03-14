522946b64459e66cef71ac4a44bf3598ff53e8dd
filipadamus <filip.adamus@onet.eu>
2023-03-14 17:49:07 +0100

    Dodanie 1 zadania które zrobiłem na uczelnie

---
+++ Projects C++/Iloczyn 2 liczb co najwyżej stucyfrowe.cpp
@@ -0,0 +1,36 @@
+﻿// Proszę napisać program, który wczytuje dwie liczby całkowite (co najwyżej stucyfrowe) a następnie wylicza i drukuje ich iloczyn.
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int a, b, suma, x = 100;
+
+	for (int i = 0; i < x; i++) {
+
+		cout << "Podaj liczbe pierwsza: ";
+		cin >> a;
+		cout << "Podaj liczbe druga: ";
+		cin >> b;
+
+		if ((a <= 100) && (b <= 100)) {
+			suma = a * b;
+			cout << "" << endl;
+			cout << "Iloczyn twoich liczb czyli " << a << " * " << b << " = " << suma << endl;
+			cout << "" << endl;
+		}
+		else {
+			cout << "" << endl;
+			cout << "Podana liczba jest wieksza od 100!" << endl;
+			cout << "Wpisz mniejsza liczbe!";
+			cout << "" << endl;
+		}
+	}
+
+	return 0;
+}
+
+/*
+   Filip Adamus
+*/
