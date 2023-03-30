7dff3c2bd1ea205e009293770e9fd2dce0b4532f
FilipAdamus03 <filip.adamus@onet.eu>
2023-03-30 18:33:36 +0200

    Projekt 8

---
+++ Projects C++/Projekt 8 - Próba Bernoulliego.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void bernoulli(bool* v, int n, double probability) {
+
+	double threshold = RAND_MAX * probability + probability;
+	for (int i = 0; i < n; i++) {
+		v[i] = rand() < threshold;
+	}
+}
+
+int main() {
+
+	int n;
+	double probability;
+
+	cin >> n >> probability;
+	bool* v = new bool[n];
+
+	srand(time(NULL));
+	bernoulli(v, n, probability);
+
+	for (int i = 0; i < n; i++) {
+		cout << v[i] << " ";
+	}
+
+	cout << endl;
+	delete[] v;
+
+	return 0;
+}
